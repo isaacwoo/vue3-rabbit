9455ced6db047247d2c4ebfc5d81edc565d1a5d7
isaac <isaacwoo13@gmail.com>
2024-05-28 11:54:13 +0900

    icon

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,12 @@
+<script setup>
+    import LayoutNav from './components/LayoutNav.vue'
+    import LayoutHeader from './components/LayoutHeader.vue'
+    import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-    <div>
-        Home page
-        <RouterView />
-    </div>   
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>

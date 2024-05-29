5ec7294f96d0d45aada3e58cf1767c86b7a077e7
isaac <isaacwoo13@gmail.com>
2024-05-29 16:30:49 +0900

    fix detail

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -54,7 +54,7 @@ onMounted(() => {
                 </li>
                 <li>
                   <p>品牌信息</p>
-                  <!-- <p>{{ goods.brand.name }}</p> -->
+                  <p>{{ goods.brand?.name }}</p>
                   <p><i class="iconfont icon-dynamic-filling"></i>品牌主页</p>
                 </li>
               </ul>
@@ -111,7 +111,7 @@ onMounted(() => {
                     </li>
                   </ul>
                   <!-- 图片 -->
-                  <!-- <img v-for="img in goods.details.pictures" :src="img" :key="img" alt="" /> -->
+                  <img v-for="img in goods.details?.pictures" :src="img" :key="img" alt="" />
                 </div>
               </div>
             </div>

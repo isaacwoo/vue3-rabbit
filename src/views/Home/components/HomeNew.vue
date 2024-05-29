1a06767aa80ccb9b177b82a7be6567bd2c54bf11
isaac <isaacwoo13@gmail.com>
2024-05-29 15:38:49 +0900

    goods route

--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -19,7 +19,7 @@ onMounted(() => {
   <HomePanel title="new" sub-title="sub new">
     <ul class="goods-list">
         <li v-for="item in newList" :key="item.id">
-        <RouterLink to="/">
+        <RouterLink :to="`/detail/${item.id}`">
             <img :src="item.picture" alt="" />
             <p class="name">{{ item.name }}</p>
             <p class="price">&yen;{{ item.price }}</p>

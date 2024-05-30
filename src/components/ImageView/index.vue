709566902ee1ce664dfafe0e6619e8e0c9822ecc
isaac <isaacwoo13@gmail.com>
2024-05-30 15:29:15 +0900

    zoom compelete

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -22,7 +22,14 @@ const { elementX, elementY, isOutside } = useMouseInElement(target)
 
 const left = ref(0)
 const top = ref(0)
-watch([elementX,elementY], () => {
+
+const positionX = ref(0)
+const positionY = ref(0)
+
+watch([elementX,elementY,isOutside], () => {
+    if(isOutside.value){
+        return
+    }
     if(elementX.value >100 && elementX.value < 300){
         left.value = elementX.value - 100
     }
@@ -42,6 +49,9 @@ watch([elementX,elementY], () => {
     if(elementY.value < 100){
         top.value = 0
     }
+
+    positionX.value = -left.value * 2
+    positionY.value = -top.value * 2
 })
 </script>
 
@@ -52,7 +62,7 @@ watch([elementX,elementY], () => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -63,11 +73,11 @@ watch([elementX,elementY], () => {
     <!-- 放大镜大图 -->
     <div class="large" :style="[
       {
-        backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
+        backgroundImage: `url(${imageList[activeIndex]})`,
+        backgroundPositionX: `${positionX}px`,
+        backgroundPositionY: `${positionY}px`,
       },
-    ]" v-show="false"></div>
+    ]" v-show="!isOutside"></div>
   </div>
 </template>
 

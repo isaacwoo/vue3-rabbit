372948b20c06612a07b22c7c6c8f237889709511
isaac <isaacwoo13@gmail.com>
2024-05-30 17:31:28 +0900

    custom vald

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -3,7 +3,8 @@ import { ref } from 'vue'
 
 const form = ref({
   account: '',
-  password: ''
+  password: '',
+  agree: true
 })
 
 const rules = {
@@ -13,6 +14,18 @@ const rules = {
   password: [
     { required: true, message: '请输入密码', trigger: 'blur' },
     { min: 6, max: 20, message: '密码长度在 6 到 20 个字符', trigger: 'blur'}
+  ],
+  agree: [
+    { 
+        validator: (rule, value, callback) => {
+            if (!value) {
+                callback(new Error('请先同意隐私条款和服务条款'))
+            } else {
+                callback()
+            }
+        },
+        trigger: 'change'
+    }
   ]
 }
 </script>
@@ -47,8 +60,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox  size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>

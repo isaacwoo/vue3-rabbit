89476f5b4c37ca73145bc857a2bbab68f85c5869
isaac <isaacwoo13@gmail.com>
2024-05-30 17:44:24 +0900

    all valid

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -28,6 +28,20 @@ const rules = {
     }
   ]
 }
+
+const formRef = ref(null)
+const doLogin = () => {
+  formRef.value.validate((valid) => {
+    if (valid) {
+      console.log('login success')
+    } else {
+      console.log('error submit!!')
+      return false
+    }
+  })
+}
+
+
 </script>
 
 
@@ -52,7 +66,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
               <el-form-item prop="account" label="账户">
                 <el-input v-model="form.account"/>
@@ -65,7 +79,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>

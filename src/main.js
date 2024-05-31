2454b64c240e21fcbbc3f9ab266ddc190cd01509
isaac <isaacwoo13@gmail.com>
2024-05-31 14:37:56 +0900

    pinia persist

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 
@@ -11,8 +11,9 @@ import { lazyPlugin } from './directives'
 import { componentPlugin } from './components'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
